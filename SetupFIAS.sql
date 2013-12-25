d6d35e03a1b3dfb45b26a23e3bcaeecb98a4058b
bosomykin <bosomykin.mm@yandex.ru>
2013-12-25 21:03:10 +0600

    Создание линка

--- SetupFIAS.sql
+++ SetupFIAS.sql
@@ -3,3 +3,17 @@
 2. Создание БД, Таблицы
 3. Наполнение данными
 */
+
+--1. Создание линка
+USE [master]
+GO
+EXEC master.dbo.sp_addlinkedserver 
+    @server = N'ASU-03-MDMDEV', 
+    @srvproduct=N'SQL Server' ;
+GO
+EXEC master.dbo.sp_addlinkedsrvlogin 
+    @rmtsrvname = N'ASU-03-MDMDEV', 
+    @useself = False , 
+    @rmtuser  = N'bdo' ,
+    @rmtpassword = '12345678';
+GO
